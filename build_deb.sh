d8b752cbe3d57fd906aa8ca8349fe41ff6977da8
ISnortSpeed <thtssoskibidi@gmail.com>
2025-04-07 15:55:52 -0400

    Update build_deb.sh
    Typo

--- build_deb.sh
+++ build_deb.sh
@@ -2,4 +2,4 @@
 echo "Building .deb package..."
 dpkg-deb --build debian
 mv debian.deb python-desktop-x11_1.0_all.deb
-echo "Done! Package is: python-desktop-x11_1.0_all.deb"
+echo "Done! Package is: flow-desktop-x11_1.0_all.deb"
