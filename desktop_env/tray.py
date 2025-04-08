db0d67053c297d611eb573b04773315bbd21c5e0
ISnortSpeed <thtssoskibidi@gmail.com>
2025-04-08 16:44:02 -0400

    Update tray.py

--- desktop_env/tray.py
+++ desktop_env/tray.py
@@ -1,3 +1,4 @@
+import psutil
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel
 from PyQt5.QtCore import QTimer, QTime
 
@@ -6,20 +7,35 @@ class SystemTray(QWidget):
         super().__init__()
         layout = QHBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
-        
-        self.clock = QLabel()
-        self.volume = QLabel("🔊")  # Placeholder
 
+        self.clock = QLabel()
+        self.volume = QLabel("🔊")
+        self.battery = QLabel()
+        self.network = QLabel()
+
+        layout.addWidget(self.network)
+        layout.addWidget(self.battery)
         layout.addWidget(self.volume)
         layout.addWidget(self.clock)
 
         self.setLayout(layout)
 
         self.timer = QTimer()
-        self.timer.timeout.connect(self.update_time)
+        self.timer.timeout.connect(self.update_status)
         self.timer.start(1000)
-        self.update_time()
+        self.update_status()
 
-    def update_time(self):
+    def update_status(self):
+        # Clock
         current_time = QTime.currentTime().toString("HH:mm")
         self.clock.setText(current_time)
+
+        # Battery
+        battery = psutil.sensors_battery()
+        if battery:
+            self.battery.setText(f"🔋 {int(battery.percent)}%")
+        else:
+            self.battery.setText("🔋 N/A")
+
+        # Network (simplified)
+        self.network.setText("📶")
